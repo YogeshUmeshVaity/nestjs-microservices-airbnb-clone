ca7a632a38b266c46c4a19743880b8bfd8c98894
Yogesh Umesh Vaity <yogeshumeshvaity@gmail.com>
2023-05-12 16:45:45 +0530

    feat(config, database): move mongodb uri to .env

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import {
+  ConfigService,
+  ConfigModule as NestConfigModule,
+} from '@nestjs/config';
 
 /**
  * The reason why we are abstracting our own ConfigModule when wrapping around NestConfigModule
@@ -9,5 +12,9 @@ import { ConfigModule as NestConfigModule } from '@nestjs/config';
  * By calling the forRoot(), we are telling the NestConfigModule to load the ENVs that we have
  * in memory and in .env files.
  */
-@Module({ imports: [NestConfigModule.forRoot()] })
+@Module({
+  imports: [NestConfigModule.forRoot()],
+  providers: [ConfigService],
+  exports: [ConfigService],
+})
 export class ConfigModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,5 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
 
-@Module({ imports: [MongooseModule.forRoot('mongodb://localhost/sleepr')] })
+/**
+ * If the app server has trouble connecting to the local MongoDB server,
+ * try 'mongodb://127.0.0.1/sleepr'.
+ *
+ * The ConfigModule here must be imported from config/config.module, not @nestjs/config.
+ */
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class DatabaseModule {}
